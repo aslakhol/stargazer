a3dec2383455d9a725f0fd623dbaa02ce1827adb
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-05 20:39:51 +0100

    lint-fix

--- src/actions/simpleAction.js
+++ src/actions/simpleAction.js
@@ -1,8 +1,8 @@
 // Placeholder
 
-export const simpleAction = () => dispatch => {
- dispatch({
-  type: 'SIMPLE_ACTION',
-  payload: 'result_of_simple_action'
- })
-}
+export const simpleAction = () => (dispatch) => {
+  dispatch({
+    type: 'SIMPLE_ACTION',
+    payload: 'result_of_simple_action',
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './reducers/rootReducer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 render(
-  <Provider store={store} >
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 serviceWorker.unregister();
