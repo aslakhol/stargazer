156dde316d994e7f97edeaceaf8bbda558cfcb67
Lars Gjelstad <largje@dhcp-10-24-1-146.wlan.ntnu.no>
2018-11-09 18:26:24 +0100

    latest changes, cards and some styling + favicon

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { connect } from 'react-redux';
 import {
   Container,
   Row,
+  Button,
   Col,
+  Popover,
+  PopoverBody,
+  PopoverHeader,
+  Input,
+  InputGroup
 } from 'reactstrap';
 import ModalExample from './components/ModalExample';
-import CardExample from './components/CardExample';
 import { simpleAction } from './actions/simpleAction';
-
+import './styles/page.css';
+import PersonCard from './components/PersonCard'
 class App extends React.Component {
   simpleAction = (event) => {
     this.props.simpleAction();
@@ -24,27 +30,35 @@ class App extends React.Component {
   }
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
+      popoverOpen: !this.state.popoverOpen
     });
   }
+
   render() {
     return (
+      // Første container, header logo etc. hamburger? 
       <div>
-        <Container>
-          <Row>
-            <Col md="3" sm="4" mb="3">
-              <CardExample className="mb-3" />
-            </Col>
-            <Col md="3" sm="4" mb="3">
-              <CardExample />
-            </Col>
+        <Container className="topHeader"> 
+        <div>
+          <Button id="usBtn" className="Popover1" onClick={this.toggle}>
+            About Us
+          </Button>
+          <Popover placement="bottom" isOpen={this.state.popoverOpen} target="usBtn" toggle={this.toggle}>
+            <PopoverHeader>About Us</PopoverHeader>
+            <PopoverBody>Studenter ved NTNU som lager fete applikasjoner i IT2810, også kjent som webdev, også kjent som the informatiker-butcher.</PopoverBody>
+          </Popover>
+        </div>
+        </Container>
+        <Container className="searchContainer">
+          <Row sm='12' md={{size: 6, offset: 3, height: 12,}}>
+            <InputGroup className="searchStyle">
+              <Input placeholder="search" />
+            </InputGroup>
           </Row>
           <Row>
             <Col md="3" sm="4" mb="3">
-              <CardExample />
-            </Col>
-            <Col md="3" sm="4" mb="3">
-              <CardExample />
+              <PersonCard className="mb-3" /> 
             </Col>
           </Row>
         </Container>
@@ -61,5 +75,10 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   simpleAction: () => dispatch(simpleAction())
 })
+
+// CLIPBOARD START
+
+
+// CLIPBOARD END
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
