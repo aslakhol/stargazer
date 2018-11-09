317e4066a3525712b23dcfe7a0d6d229207adc66
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-09 20:08:57 +0100

    remove popover

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { connect } from 'react-redux';
 import {
   Container,
   Row,
-  Button,
   Col,
-  Popover,
-  PopoverBody,
-  PopoverHeader,
   Input,
   InputGroup,
 } from 'reactstrap';
@@ -20,38 +16,23 @@ export class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggle = () => this.toggle();
+    this.toggleIsOpen = () => this.toggleIsOpen(); // I don't know about this.
+
     this.state = {
       isOpen: false,
     };
   }
 
-  toggle() {
+  toggleIsOpen() {
     this.setState(prevState => ({
       isOpen: !prevState.isOpen,
-      popoverOpen: !prevState.popoverOpen,
     }));
   }
 
   render() {
-    const { popoverOpen } = this.props;
     return (
       // Første container, header logo etc. hamburger?
       <div>
-        <Container className="topHeader">
-          <div>
-            <Button id="usBtn" className="Popover1" onClick={this.toggle}>
-              About Us
-            </Button>
-            <Popover placement="bottom" isOpen={popoverOpen} target="usBtn" toggle={this.toggle}>
-              <PopoverHeader>About Us</PopoverHeader>
-              <PopoverBody>
-                Studenter ved NTNU som lager fete applikasjoner i IT2810,
-                 også kjent som webdev, også kjent som the informatiker-butcher.
-              </PopoverBody>
-            </Popover>
-          </div>
-        </Container>
         <Container className="searchContainer">
           <Row sm="12" md={{ size: 6, offset: 3, height: 12 }}>
             <InputGroup className="searchStyle">
