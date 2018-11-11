f32a6e8c63b8bd2532c34b1d3877f44fd6a5798e
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-11 19:16:04 +0100

    create tests for reducers

---
+++ src/__tests__/reducerTests/personReducer.test.js
@@ -0,0 +1,27 @@
+import personReducer from '../../reducers/personReducer';
+import { SET_CURRENT_PERSON } from '../../actions/types';
+
+const mockPerson = {
+  name: 'C-3PO',
+  url: 'https://swapi.co/api/people/2/',
+};
+
+describe('personReducer', () => {
+  describe('INITIAL_STATE', () => {
+    it('is correct', () => {
+      const action = { type: 'DUMMY_ACTION' };
+      const initialState = {};
+
+      expect(personReducer(undefined, action)).toEqual(initialState);
+    });
+  });
+
+  describe('SET_CURRENT_PERSON', () => {
+    it('returns the correct state', () => {
+      const action = { type: SET_CURRENT_PERSON, data: mockPerson };
+      const expectedState = mockPerson;
+
+      expect(personReducer(undefined, action)).toEqual(expectedState);
+    });
+  });
+});
