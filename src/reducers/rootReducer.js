9f834f2e76204ff24433001a9d021abc0fcf9a26
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-11 19:15:48 +0100

    remove simpleReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import simpleReducer from './simpleReducer';
 import personReducer from './personReducer';
 import modalReducer from './modalReducer';
 
 export default combineReducers({
-  simpleReducer,
   currentPerson: personReducer,
   modalOpen: modalReducer,
 });
