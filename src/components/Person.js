4a9a748454a4c57653a76e82219a806adec601f9
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-12 23:22:04 +0100

    please the linter

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const Person = props => (
   <h3>
+    {/* Linting is ignored only because this component will soon be rewritten. */}
+    {/* eslint-disable-next-line react/destructuring-assignment */}
     {JSON.stringify(props.items)}
   </h3>
 );
