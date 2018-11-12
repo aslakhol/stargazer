948750bf3fe4abcf0c254a386a9af7d1be54f4e3
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-12 19:45:19 +0100

    combine App and TheApp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import {
   Container,
+  Col,
   Row,
   Input,
   InputGroup,
 } from 'reactstrap';
+import Search from './components/Search';
+import Person from './components/Person';
 
+import { fetchPersonsIfNeeded } from './actions/actions';
 import './styles/page.css';
 import SearchPageContainer from './containers/SearchPageContainer';
 
@@ -16,9 +20,21 @@ export class App extends React.Component {
 
     this.toggleIsOpen = () => this.toggleIsOpen(); // I don't know about this.
 
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
       isOpen: false,
     };
+  }
+
+  componentDidMount() {
+    const { dispatch, query } = this.props;
+    dispatch(fetchPersonsIfNeeded(query));
+  }
+
+  handleChange(query) {
+    const { dispatch } = this.props;
+    dispatch(fetchPersonsIfNeeded(query));
   }
 
   toggleIsOpen() {
@@ -27,7 +43,16 @@ export class App extends React.Component {
     }));
   }
 
+
   render() {
+    const {
+      query,
+      items,
+      isFetching,
+    } = this.props;
+
+    console.log(this.props);
+
     return (
       <Container className="searchContainer">
         <Row sm="12" md={{ size: 6, offset: 3, height: 12 }} className="my-3 mx-0">
@@ -38,17 +63,42 @@ export class App extends React.Component {
         <Row className="mb-3">
           <SearchPageContainer />
         </Row>
+        <Row>
+          <Search value={query} onChange={this.handleChange} />
+        </Row>
+        <Row>
+          <Col md="12">
+            {/* {isFetching && items.length === 0
+              && <h2>Loading</h2>}
+
+            {!isFetching && items.length <= 0
+              && <h2>No results</h2>}
+
+            {items.length > 0
+              && <Person items={items} />
+            } */}
+          </Col>
+        </Row>
       </Container>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  ...state,
-});
+const mapStateToProps = (state) => {
+  const { query, personsByQuery } = state;
+  const { isFetching, items } = personsByQuery[query] || {
+    query,
+    isFetching: true,
+    items: [],
+  };
+  return {
+    query,
+    items,
+    isFetching,
+  };
+};
 
-const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(),
+const mapDispatchToProps = () => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
