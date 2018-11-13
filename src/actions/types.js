563ac6dd5c6976bb7ae55060e110c9475e078bff
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-13 19:36:17 +0100

    moved action types to types

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,3 +4,8 @@ export const CLOSE_MODAL = 'CLOSE_MODAL';
 
 // Data Related
 export const SET_CURRENT_PERSON = 'SET_CURRENT_PERSON';
+
+// Query related
+export const REQUEST_PERSON = 'REQUEST_PERSON';
+export const RECIEVE_PERSON = 'RECIEVE_PERSON';
+export const NEW_QUERY = 'NEW_QUERY';
