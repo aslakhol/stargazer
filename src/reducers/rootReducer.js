7a00a0a49135c9c0fc1ada4e3af6247ee576de54
Adrian <adrianhthompson@gmail.com>
2018-11-14 21:13:49 +0100

    added historyReducer to rootreducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,10 +3,12 @@ import personReducer from './personReducer';
 import modalReducer from './modalReducer';
 import searchReducer from './searchReducer';
 import timeoutReducer from './timeoutReducer';
+import historyReducer from './searchHistoryReducer';
 
 export default combineReducers({
   currentPerson: personReducer,
   modalOpen: modalReducer,
   request: searchReducer,
   timeout: timeoutReducer,
+  history: historyReducer,
 });
