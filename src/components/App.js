8802536a43a41aa7cc4ee89e8c5923329ee4beae
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-14 11:56:53 +0100

    rewrite app to be functional and get rid of superflous container

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,17 @@ import {
 import SearchPageContainer from '../containers/SearchPageContainer';
 import SearchBarContainer from '../containers/SearchBarContainer';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <Container className="searchContainer">
-        <Row className="my-3 mx-0">
-          <SearchBarContainer />
-        </Row>
-        <Row className="mb-3">
-          <SearchPageContainer />
-        </Row>
-      </Container>
-    );
-  }
+function App() {
+  return (
+    <Container>
+      <Row className="my-3 mx-0">
+        <SearchBarContainer />
+      </Row>
+      <Row className="mb-3">
+        <SearchPageContainer />
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
