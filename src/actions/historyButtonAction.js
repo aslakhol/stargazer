388505f88ce827c24cc95c614923924f915a2ab4
Adrian <adrianhthompson@gmail.com>
2018-11-14 19:11:12 +0100

    added buttonaction

---
+++ src/actions/historyButtonAction.js
@@ -0,0 +1,8 @@
+import { SHOW_HISTORY, HIDE_HISTORY } from '../constants';
+
+export const showHistory = () => ({ type: SHOW_HISTORY });
+export const hideHistory = () => ({ type: HIDE_HISTORY });
+
+export const clickHistoryButton = () => {
+  showHistory();
+};
