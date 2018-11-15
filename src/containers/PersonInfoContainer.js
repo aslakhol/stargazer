4c34dbcd989d1e865eab733cdc2dc1fb4263739e
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-15 18:04:32 +0100

    last cleanup

--- src/containers/PersonInfoContainer.js
+++ src/containers/PersonInfoContainer.js
@@ -1,19 +1,12 @@
 import { connect } from 'react-redux';
 import renderPersonInfo from '../components/PersonInfo';
-import { clickPersonCard, pressEnterPersonCard } from '../actions/multiActions';
 
 const mapStateToProps = (state, ownProps) => ({
   person: ownProps.person,
-  cardCondition: ownProps.cardCondition,
-});
-
-const mapDispatchToProps = dispatch => ({
-  onClick: data => dispatch(clickPersonCard(data)),
-  handleKeyPress: (event, data) => dispatch(pressEnterPersonCard(event, data)),
 });
 
 const PersonCardContainer = connect(
-  mapStateToProps, mapDispatchToProps,
+  mapStateToProps,
 )(renderPersonInfo);
 
 export default PersonCardContainer;
