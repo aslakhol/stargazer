b9c67d98e1bebc795d7376261c56b2f88aa31ed9
Adrian <adrianhthompson@gmail.com>
2018-11-16 20:04:33 +0100

    some more test

--- cypress/integration/sort_by_spec.js
+++ cypress/integration/sort_by_spec.js
@@ -4,9 +4,16 @@ describe("Tests the filtering", () => {
     cy.visit("localhost:3000")
   })
 
-  it("Searches for Lars and sorts by name", () => {
+  it("Searches for Skywalker and sorts by name", () => {
     cy.get(".form-control")
       .click()
-      .type("Lars")  
+      .type("Skywalker")
+      cy.get("div.mb-3.row")
+      .children().should("be.visible")
+      .get(".btn-group").contains("Name").click()
+      cy.get("div.mb-3.row")
+      .children().should("be.visible")
+
+
     })
 })
