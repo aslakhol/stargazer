2f620af556d90eba87fecbb9d71cdf0bca6d1b75
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-16 21:58:18 +0100

    create component tests

--- src/__tests__/componentTests/App.test.js
+++ src/__tests__/componentTests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import App from '../components/App';
+import App from '../../components/App';
 
 describe('<App />', () => {
   describe('render()', () => {
