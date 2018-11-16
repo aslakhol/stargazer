7ec564e07762384947035736473723aa826b8e7f
Adrian <adrianhthompson@gmail.com>
2018-11-16 19:16:17 +0100

    adds searchBarTest

--- cypress/integration/search-bar_spec.js
+++ cypress/integration/search-bar_spec.js
@@ -1,22 +1,27 @@
 describe("Tests the searchbar", () => {
 
-  it("Focuses the searchbar on load",  () => {
+  beforeEach(() => {
     cy.visit("localhost:3000")
-    
+  })
+
+  it("Focuses the searchbar on load",  () => {
     cy.focused()
       .should("have.class", "form-control")
   })
 
-  it("Accepts input", () => {
-    cy.visit("localhost:3000")
-
+  it("Accepts input and shows output", () => {
     cy.get(".form-control").click()
       .type("Darth Vader")
       .should("have.value", "Darth Vader")
-      
   })
 
-    
+  it("Types input and deletes it", () => {
+    cy.get(".form-control").click()
+      .type("Darth Vader")
+      .focused().clear()
+    cy.get("div.mb-3.row").children().should("be.visible")
+  })
+  
 })
 
 
