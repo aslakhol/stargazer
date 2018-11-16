27a951d481d345e805b5e9a63cfbeafc885f3e1e
Aslak Hollund <aslakho@stud.ntnu.no>
2018-11-16 13:53:32 +0100

    rename filterActions -> buttonActions and create actions for sorting

--- src/actions/buttonActions.js
+++ src/actions/buttonActions.js
@@ -1,4 +1,5 @@
-import { INCLUDE_SELECTED, EXCLUDE_SELECTED } from '../utils/constants';
+import { INCLUDE_SELECTED, EXCLUDE_SELECTED, SORT_BY_SELECTED } from '../utils/constants';
 
 export const includeSelectedInQuery = selected => ({ type: INCLUDE_SELECTED, selected });
 export const excludeSelectedInQuery = selected => ({ type: EXCLUDE_SELECTED, selected });
+export const sortBySelected = selected => ({ type: SORT_BY_SELECTED, selected });
